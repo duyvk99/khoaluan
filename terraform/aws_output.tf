433e49570583d68392919db3687dc27bdab5a6fb
duyvk <duyvk@etrustcloud.net>
2021-07-24 00:11:35 +0700

    delete old code

--- terraform/aws_output.tf
+++
@@ -1,4 +0,0 @@
-output "instance_ip_public" {
-  value = aws_instance.web-server.public_ip
-  description = "Địa chỉ IP public của instance"
-}
--- terraform/aws_provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  profile    = "default"
-  region     = "ap-southeast-1" //khu vuc singapore
-  access_key = "<put access key here>"
-  secret_key = "<put secret key here >"
-}
