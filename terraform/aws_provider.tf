efc0d428343a1d5e66ab1f21a8530f8e3b1c9dcf
duyvk <duyvk@etrustcloud.net>
2021-07-25 19:46:55 +0700

    terraform required

--- terraform/aws_provider.tf
+++ terraform/aws_provider.tf
@@ -4,11 +4,3 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
